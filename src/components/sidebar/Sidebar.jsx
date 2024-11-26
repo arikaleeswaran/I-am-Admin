c9212a74a77d4b163312da5647a8169973564c30
arikaleeswaran <g.arikaleeswaran7@gmail.com>
2024-11-26 19:07:51 +0530

    Added sidebar and container and added icons

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,82 @@
+import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="top">
+            <span className="logo">I am Admin</span>
+        </div>
+        <hr/>
+        <div className="center">
+            <ul>
+                <p className="title">MAIN</p>
+                <li>
+                    <DashboardIcon className="icon"/>
+                    <span>Dashboard</span>
+                </li>
+                <p className="title">LISTS</p>
+                <li>
+                    <AccountCircleOutlinedIcon className="icon"/>
+                    <span>Users</span>
+                </li>
+                <li>
+                    <StoreIcon className="icon"/>
+                    <span>Products</span>
+                </li>
+                <li>
+                    <CreditCardIcon className="icon"/>
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingIcon className="icon"/>
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <InsertChartIcon className="icon"/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <NotificationsNoneIcon className="icon"/>
+                    <span>Notifications</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <SettingsSystemDaydreamIcon className="icon"/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <PsychologyIcon className="icon"/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+                <p className="title">USER</p>
+                <li>
+                    <AccountCircleOutlinedIcon className="icon"/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <ExitToAppOutlinedIcon className="icon"/>
+                    <span>Logout</span>
+                </li>
+            </ul>
+        </div>
+        <div className="bottom">color options</div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import "./home.scss"
 
 function Home() {
   return (
-    <div className='home'>
-        Home
+    <div className="home">
+        <Sidebar/>
+        <div className="homeContainer">Container</div>
     </div>
   )
 }
