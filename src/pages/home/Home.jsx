0478bc3c3c54a1a539ed3c39418c9378ef3139b5
arikaleeswaran <g.arikaleeswaran7@gmail.com>
2024-11-27 00:26:31 +0530

    Adding Progressbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.scss"
 import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
 
 function Home() {
   return (
@@ -15,6 +17,10 @@ function Home() {
             <Widget type="order"/>
             <Widget type="earning"/>
             <Widget type="balance"/>
+          </div>
+          <div className="charts">
+            <Featured/>
+            <Chart/>
           </div>
         </div>
     </div>
